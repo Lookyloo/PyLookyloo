7e43efbc44032c45df76ff81bb64ed1900b0bc88
Raphaël Vinot <raphael@vinot.info>
2020-11-02 15:43:43 +0100

    new: Add docstring.

--- pylookyloo/api.py
+++ pylookyloo/api.py
@@ -12,6 +12,10 @@ import requests
 class Lookyloo():
 
     def __init__(self, root_url: str='https://lookyloo.circl.lu/'):
+        '''Query a specific lookyloo instance.
+
+        :param root_url: URL of the instance to query.
+        '''
         self.root_url = root_url
         if not self.root_url.endswith('/'):
             self.root_url += '/'
@@ -19,11 +23,13 @@ class Lookyloo():
 
     @property
     def is_up(self) -> bool:
+        '''Test if the given instance is accessible'''
         r = self.session.head(self.root_url)
         return r.status_code == 200
 
     def enqueue(self, url: Optional[str]=None, quiet: bool=False, **kwargs) -> str:
         '''Enqueue an URL.
+
         :param url: URL to enqueue
         :param quiet: Returns the UUID only, instead of the whole URL
         :param kwargs: accepts all the parameters supported by `Lookyloo.scrape`
@@ -42,37 +48,75 @@ class Lookyloo():
             return urljoin(self.root_url, f'tree/{response.text}')
 
     def get_redirects(self, capture_uuid: str) -> Dict[str, Any]:
+        '''Returns the initial redirects.
+
+        :param capture_uuid: UUID of the capture
+        '''
         r = self.session.get(urljoin(self.root_url, str(Path('json', capture_uuid, 'redirects'))))
         return r.json()
 
     def get_screenshot(self, capture_uuid: str) -> BytesIO:
+        '''Returns the screenshot.
+
+        :param capture_uuid: UUID of the capture
+        '''
         r = self.session.get(urljoin(self.root_url, str(Path('tree', capture_uuid, 'image'))))
         return BytesIO(r.content)
 
     def get_cookies(self, capture_uuid: str) -> List[Dict[str, str]]:
+        '''Returns the complete cookies jar.
+
+        :param capture_uuid: UUID of the capture
+        '''
         r = self.session.get(urljoin(self.root_url, str(Path('tree', capture_uuid, 'cookies'))))
         return r.json()
 
     def get_html(self, capture_uuid: str) -> StringIO:
+        '''Returns the rendered HTML as it would be in the browser after the page loaded.
+
+        :param capture_uuid: UUID of the capture
+        '''
         r = self.session.get(urljoin(self.root_url, str(Path('tree', capture_uuid, 'html'))))
         return StringIO(r.text)
 
     def get_hashes(self, capture_uuid: str) -> StringIO:
+        '''Returns all the hashes of all the bodies (including the embedded contents)
+
+        :param capture_uuid: UUID of the capture
+        '''
         r = self.session.get(urljoin(self.root_url, str(Path('tree', capture_uuid, 'hashes'))))
         return StringIO(r.text)
 
     def get_complete_capture(self, capture_uuid: str) -> BytesIO:
+        '''Returns a zip files that contains the screenshot, the har, the rendered HTML, and the cookies.
+
+        :param capture_uuid: UUID of the capture
+        '''
         r = self.session.get(urljoin(self.root_url, str(Path('tree', capture_uuid, 'export'))))
         return BytesIO(r.content)
 
     def get_hash_occurrences(self, h: str) -> Dict[str, Any]:
+        '''Returns the base 64 body related the the hash, and a list of all the captures containing that hash.
+
+        :param h: sha512 to search
+        '''
         r = self.session.get(urljoin(self.root_url, str(Path('json', 'hash_info', h))))
         return r.json()
 
     def get_url_occurrences(self, url: str) -> Dict[str, Any]:
+        '''Returns all the captures contining the URL
+
+        :param url: URL to lookup
+        '''
         r = self.session.post(urljoin(self.root_url, str(Path('json', 'url_info'))), json={'url': url})
         return r.json()
 
-    def get_hostname_occurrences(self, url: str, with_urls_occurrences: bool=False) -> Dict[str, Any]:
-        r = self.session.post(urljoin(self.root_url, str(Path('json', 'hostname_info'))), json={'hostname': url, 'with_urls_occurrences': with_urls_occurrences})
+    def get_hostname_occurrences(self, hostname: str, with_urls_occurrences: bool=False) -> Dict[str, Any]:
+        '''Returns all the captures contining the hostname. It will be pretty slow on very common domains.
+
+        :param hostname: Hostname to lookup
+        :param with_urls_occurrences: If true, add details about the related URLs.
+        '''
+        r = self.session.post(urljoin(self.root_url, str(Path('json', 'hostname_info'))), json={'hostname': hostname,
+                                                                                                'with_urls_occurrences': with_urls_occurrences})
         return r.json()
