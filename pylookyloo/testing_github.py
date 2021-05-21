d79fb905badb2b9f12d37a59a90454929b07c931
felalex57 <alexandre.felix@live.fr>
2021-05-21 13:46:58 +0200

    fix: instance name

--- pylookyloo/testing_github.py
+++ pylookyloo/testing_github.py
@@ -7,15 +7,15 @@ from pylookyloo import Lookyloo
 
 class UnitTesting(unittest.TestCase):
 
-    public_instance: Lookyloo
+    github_instance: Lookyloo
 
     @classmethod
     def setUpClass(cls) -> None:
-        setattr(cls, "public_instance", Lookyloo('http://127.0.0.1:5100'))
+        setattr(cls, "github_instance", Lookyloo('http://127.0.0.1:5100'))
 
     # Check that lookyloo.circl.lu is up
-    def test_public_instance_is_up(self) -> None:
-        self.assertTrue(self.public_instance.is_up)
+    def test_github_instance_is_up(self) -> None:
+        self.assertTrue(self.github_instance.is_up)
 
 
 if __name__ == '__main__':
